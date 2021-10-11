0d374eaa3db1f9bee3dc9b27ef4c66f73f3ca9ec
Haziq <60842832+n3r052@users.noreply.github.com>
2021-10-11 18:16:40 +0800

    updated php command with inner join

--- profile.php
+++ profile.php
@@ -1,5 +1,12 @@
 <?php
     require 'config.php';
+    $sql = "SELECT user.user_id, user.first_name, user.last_name, user.email, user.status, user.position, user.company, profile.about, profile.profile, CONCAT(user.first_name, ' ', user.last_name) AS whole_name FROM user
+            INNER JOIN profile on user.user_id = profile.user_id
+            WHERE user.user_id = 3001";
+                    $result = $connect->query($sql);
+                    if ($result->num_rows > 0) {
+                        // output data of each row
+                        while($row = $result->fetch_assoc()) {
 ?>
 <!DOCTYPE html>
 <html>
@@ -157,7 +164,11 @@
         <div class="container-fluid d-flex align-items-center">
           <div class="row">
             <div class="col-lg-7 col-md-10">
-              <h1 class="display-2 text-white">Hello John Doe</h1>
+              <h1 class="display-2 text-white">Hello 
+                  <?php 
+                    echo $row['whole_name']; 
+                  ?>
+                </h1>
               <p class="text-white mt-0 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo eos ducimus, iure aut itaque mollitia impedit necessitatibus corporis a saepe deserunt! Quibusdam nostrum, eum maiores esse voluptate doloremque laudantium.</p>       
             </div>
           </div>
@@ -175,7 +186,7 @@
                 <div class="col-lg-3 order-lg-2">
                   <div class="card-profile-image">
                     <a href="#">
-                      <img src="assets/img/theme/team-6.jpg" class="rounded-circle">
+                      <img src="<?php echo $row['profile']; ?>" class="rounded-circle">
                     </a>
                   </div>
                 </div>
@@ -189,23 +200,27 @@
                   <div class="col">
                     <div class="card-profile-stats d-flex justify-content-center">
                       <div>
-                        <span class="heading">John Doe</span>  
-                       
-                      </div>
-                      <!-- <div>
-                        <span class="heading">Type</span>
-                        
-                      </div> -->
-                      
+                        <span class="heading">
+                        <?php 
+                            echo $row['whole_name']; 
+                        ?>    
+                        </span>  
+                      </div>                      
                     </div>
                   </div>
                 </div>
                 <div class="text-center">
                   <div class="h5 font-weight-300">
-                    <i class="ni location_pin mr-2"></i>User ID - Email
+                    <i class="ni location_pin mr-2"></i>
+                    <?php 
+                        echo $row['user_id']." - ".$row['email']; 
+                    ?>
                   </div>
                   <div class="h5 mt-4">
-                    <i class="ni business_briefcase-24 mr-2"></i>Admin - Company
+                    <i class="ni business_briefcase-24 mr-2"></i>
+                    <?php 
+                        echo $row['position']." - ".$row['company']; 
+                    ?>
                   </div>
                 </div>
               </div>
@@ -226,75 +241,38 @@
                 <form>
                   <h6 class="heading-small text-muted mb-4">User information</h6>
                   <div class="pl-lg-4">
-                    <div class="row">
-                      <div class="col-lg-6">
-                        <div class="form-group">
-                          <label class="form-control-label" for="input-username">Username</label>
-                          <input type="text" id="input-username" class="form-control" placeholder="Username" value="John Doe">
-                        </div>
-                      </div>
-                      <div class="col-lg-6">
-                        <div class="form-group">
-                          <label class="form-control-label" for="input-email">Email address</label>
-                          <input type="email" id="input-email" class="form-control" placeholder="john@example.com">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
+                  <div class="row">
                       <div class="col-lg-6">
                         <div class="form-group">
                           <label class="form-control-label" for="input-first-name">First name</label>
-                          <input type="text" id="input-first-name" class="form-control" placeholder="First name" value="John">
+                          <input type="text" id="input-first-name" class="form-control" placeholder="<?php echo $row['first_name']; ?>" value="">
                         </div>
                       </div>
                       <div class="col-lg-6">
                         <div class="form-group">
                           <label class="form-control-label" for="input-last-name">Last name</label>
-                          <input type="text" id="input-last-name" class="form-control" placeholder="Last name" value="Doe">
+                          <input type="text" id="input-last-name" class="form-control" placeholder="<?php echo $row['last_name']; ?>" value="">
                         </div>
                       </div>
                     </div>
+
+                    <div class="row">
+                      <div class="col-lg-6">
+                        <div class="form-group">
+                          <label class="form-control-label" for="input-email">Email address</label>
+                          <input type="email" id="input-email" class="form-control" placeholder="<?php echo $row['email']; ?>">
+                        </div>
+                      </div>
+                    </div>
+
                   </div>
-                  <!-- <hr class="my-4" />
-                  Address
-                  <h6 class="heading-small text-muted mb-4">Contact information</h6>
-                  <div class="pl-lg-4">
-                    <div class="row">
-                      <div class="col-md-12">
-                        <div class="form-group">
-                          <label class="form-control-label" for="input-address">Address</label>
-                          <input id="input-address" class="form-control" placeholder="Home Address" value="" type="text">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-lg-4">
-                        <div class="form-group">
-                          <label class="form-control-label" for="input-city">City</label>
-                          <input type="text" id="input-city" class="form-control" placeholder="City" value="New York">
-                        </div>
-                      </div>
-                      <div class="col-lg-4">
-                        <div class="form-group">
-                          <label class="form-control-label" for="input-country">Country</label>
-                          <input type="text" id="input-country" class="form-control" placeholder="Country" value="United States">
-                        </div>
-                      </div>
-                      <div class="col-lg-4">
-                        <div class="form-group">
-                          <label class="form-control-label" for="input-country">Postal code</label>
-                          <input type="number" id="input-postal-code" class="form-control" placeholder="Postal code">
-                        </div>
-                      </div>
-                    </div>
-                  </div> -->
                   <hr class="my-4" />
                   <!-- Description -->
                   <h6 class="heading-small text-muted mb-4">About me</h6>
                   <div class="pl-lg-4">
                     <div class="form-group">
                       <label class="form-control-label">About Me</label>
-                      <textarea rows="4" class="form-control" placeholder="A few words about you ...">A Hoshiyomi</textarea>
+                      <textarea rows="4" class="form-control" placeholder="<?php echo $row['about']; ?>"></textarea>
                     </div>
                   </div>
                   <!-- Form Button Modal, Trigger ----------------------------------------------------------->
@@ -325,6 +303,12 @@
             </div>
           </div>
         </div>
+        <?php
+                }
+            }else{
+                echo "0 results";
+            }
+        ?>
         <!-- Footer -->
         <footer class="footer pt-0">
           <div class="row align-items-center justify-content-lg-between">
