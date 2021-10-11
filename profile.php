d97abf7684098cd647d4012107d41511bd92216d
Haziq <60842832+n3r052@users.noreply.github.com>
2021-10-11 22:39:48 +0800

    added session variable for profile page

--- profile.php
+++ profile.php
@@ -1,7 +1,10 @@
 <?php
     require 'config.php';
     session_start();
-
+    $username = $_SESSION["username"];
+    $position = $_SESSION['position'];
+    $userid = $_SESSION["userid"];
+    
     $sql = "SELECT user.user_id, user.first_name, user.last_name, user.email, user.status, user.position, user.company, profile.about, profile.profile, CONCAT(user.first_name, ' ', user.last_name) AS whole_name FROM user
             INNER JOIN profile on user.user_id = profile.user_id
             WHERE user.user_id = 3001";
