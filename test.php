e3707b09ee2088988e443fd6fb08e11a3b092d9b
Haziq <60842832+n3r052@users.noreply.github.com>
2021-10-11 19:30:33 +0800

    added header once logged in succesfully

--- test.php
+++ test.php
@@ -132,7 +132,7 @@
                 $count = mysqli_num_rows($result);  
                   
                 if($count == 1){  
-                    echo "<h1><center> Login successful </center></h1>";  
+                    header("location: index.php");  
                 }  
                 else{  
                     echo "<h1> Login failed. Invalid username or password.</h1>";  
