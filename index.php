780652e5d0c33f7473cc2a95882d06cdff1dca45
Haziq <60842832+n3r052@users.noreply.github.com>
2021-10-11 20:14:17 +0800

    added logout php command

--- index.php
+++ index.php
@@ -159,7 +159,7 @@
                   <span>Settings</span>
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#!" class="dropdown-item">
+                <a href="modal/logout.php" class="dropdown-item">
                   <i class="ni ni-user-run"></i>
                   <span>Logout</span>
                 </a>
