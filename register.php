789037150e4820a37537481755f978fca7ae3c11
19ftt1185 <19ftt1185@student.pb.edu.bn>
2021-10-12 21:56:27 +0800

    redesign

--- register.php
+++ register.php
@@ -1,84 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create New Account</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
+  <title>Create new Account</title>
+  <!-- Favicon -->
+  <link rel="icon" href="assets/img/brand/favicon.png" type="image/png">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+  <!-- Icons -->
+  <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <!-- Argon CSS -->
+  <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css">
 </head>
-<style>
-body a { text-decoration: none; color: inherit; }
-.header { grid-area: header; background-color: white; border-bottom: 2px solid black;
-                height: 60px;
-                }
-#item1 {
-            float:left;
-            margin: 10px 0 0 20px;
-            text-align: center;
-        }   #item1 a { float: left; margin: 10px 20px; font-size: 20px; font-weight: bold; }
-            #item1 img { float: left; }
-#item2 {
-            float: right;
-            margin: 10px 0 0 0;
-        } #item2 input { float: left; margin: 10px 20px; }
-        #selected { position: relative; background-color: #696969; padding: 2px 10px 2px 10px; margin-bottom: 20px;}
-#item3 { grid-area: item3;  background-color: #696969;
-                text-align: center; 
-                }  #item3 h3 {  width: 350px; margin: 30px auto 0 auto; border-bottom: 2px solid #fff;}
-                #item3p { width: 350px; margin: 0 auto 0 auto;}
-#item4 { grid-area: item4; background-color: #696969; 
-                text-align: center;
-                }  #item4 h3 { width: 350px; margin: 30px auto 20px auto; border-bottom: 2px solid #fff;}
-#item5 { grid-area: item5; background-color: #696969; 
-                }  #form { padding-left: 30px;}
-.footer { grid-area: footer; background-color: #696969;
-                display: grid;
-                grid-template-areas: 
-                'item3 item4 item5';
-                height: 300px;
-                color: #fff;
-                }
-.main { height: 1300px; background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url("./img/background.jpg"); 
-        width: 1440px; background-size: 100% 100%; background-color: #696969;}
-.grid-container {
-            display: grid;
-            grid-template-areas: 
-            'header header header header header header'
-            'main main main main main main'
-            'footer footer footer footer footer footer'
-            ;
-            max-height: 1000px;
-        }
-
-.nicebody {
-    margin-left: auto; margin-right: auto;
-    margin-top: 135px;
-    background-color: #A9A9A9;
-    width: 650px;
-    height: 1100px;
-}   
-.subjects { 
-    font-size: 30px; 
-    padding: 10px 0 0 30px;
-}   .subjects input { width: 550px; }
-    .subjects p { color: darkslategray; }
-    .subjects a { color: darkslategray; }
-#submit { width: 100px; float: right; margin-right: 50px;}
-#title {
-    text-align: center;
-    background-color: grey;
-    height: 140px;
-    width: 650px;
-    font-size: 35px;
-    font-weight: bold;
-    border: 2px solid black;
-}
-input.submit {
-    margin-right: 5px;
-}
-
-</style>
-
 
 <script>
 function myFunction() {
@@ -86,30 +23,55 @@ function myFunction() {
 }
                 </script>
 
-<body>
-    <div class="grid-container">
-
-        <div class="main">
-            <div class="nicebody">
-                <div id="title">
-                    <h2>Create New Account</h2>
-                </div>
-                    <form action="register.php" method="post">
-                    <div class="subjects">
-
-                    <label for="email">Email:</label><br>
-                    <input type="text" id="email" name="email"></input><br><br>
-                    <label for="PASSWORD">Password:</label><br>
-                    <input type="password" id="PASSWORD" name="PASSWORD"></input><br><br>
-
-                    <p>By creating new account, you are agreeing to our  terms and conditions.</p>
-                    <a href="">Already sign up? Sign in</a>
-                    <input class="submit" type="submit" value="Submit" name="submit" id="submit" onclick ="myFunction()" /><br><br>
-                    </form>
-                </div>
-            </div>
-        </div>
+<body class="bg-default">
+  
+  <!-- Main content -->
+  <div class="main-content">
+    <!-- Header -->
+    <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
     </div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
+      <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg-secondary border-0 mb-0">
+                    <div class="card-body px-lg-5 py-lg-5">
+
+                        <h2>Create New Account</h2>
+                            <form action="register.php" method="post">
+
+                            <div class="form-group mb-3">
+                            <div class="input-group input-group-merge input-group-alternative">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                </div>
+                                <input class="form-control" placeholder="Email" type="text" id="email" name="email">
+                            </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="input-group input-group-merge input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Password" type="text" id="PASSWORD" name="PASSWORD">
+                                </div>
+                            </div>
+                                    <p>By creating new account, you are agreeing to our  terms and conditions.</p>
+                            <a href="">Already sign up? Sign in</a>
+                            
+
+                            <div class="text-center">
+                            <input class="submit" type="submit" value="Submit" name="submit" id="submit" onclick ="myFunction()" /><br><br>
+                                <button type="button" class="btn btn-primary my-4" name="submit">Submit</button>
+                            </div>
+                        </form>
+                 </div>
 
     <?php
     if (isset($_POST['submit'])){
