00778e66386c721f954267d81ba435ee0810919c
Haziq <60842832+n3r052@users.noreply.github.com>
2021-10-12 12:41:26 +0800

    added profile session and remove unused command

--- officer/index.php
+++ officer/index.php
@@ -4,14 +4,16 @@
   $username = $_SESSION["username"];
   $position = $_SESSION['position'];
   $userid = $_SESSION["userid"];
-  
-  $sql = "SELECT user.user_id, user.first_name, user.last_name, user.email, user.status, user.position, user.company, profile.about, profile.profile, CONCAT(user.first_name, ' ', user.last_name) AS whole_name FROM user
-          INNER JOIN profile on user.user_id = profile.user_id";  
-          $result = mysqli_query($connect, $sql);  
-          $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-          $count = mysqli_num_rows($result);
+  $profile_pic = $_SESSION["profile_pic"];
 
-          $profile_pic = $row['profile'];
+  // $sql = "SELECT user.user_id, user.first_name, user.last_name, user.email, user.status, user.position, user.company, profile.about, profile.profile, CONCAT(user.first_name, ' ', user.last_name) AS whole_name FROM user
+  //         INNER JOIN profile on user.user_id = profile.user_id
+  //         WHERE user.user_id = '$userid'";  
+  //         $result = mysqli_query($connect, $sql);  
+  //         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+  //         $count = mysqli_num_rows($result);
+
+  //         $profile_pic = $row['profile'];
 ?>
 <!DOCTYPE html>
 <html>
