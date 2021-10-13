f4ce64abef56d82c989cd43733ce1634c58458d0
19ftt1185 <19ftt1185@student.pb.edu.bn>
2021-10-13 14:46:03 +0800

    rearranged the report format

--- form.php
+++ form.php
@@ -31,11 +31,18 @@
     $pdf->SetFont('Times','',12);
 
 
-    $pdf->Cell(80,10,'Date:',0,0);
-    $pdf->Cell(0,10,$date_time,0,1);
+    $pdf->Cell(10,10,'Date:',0,0);
+    $pdf->Cell(10,10,$date_time,0,1);
 
-    $pdf->Cell(80,10,'Name:',0,0);
-    $pdf->Cell(0,10,$first_name,0,1);
+    $pdf->Cell(10,10,'Dear',0,0);
+    $pdf->Cell(10,10,$first_name,0,1);
+
+
+    $pdf->Cell(80,10,'This notice is to inform you that you have been cited with a traffic violation and must pay your citation ',0,1);
+    $pdf->Cell(0,10,'within 72hours. We have payment plans available if necessary.',0,0);
+    
+
+
 
     // $pdf->Image('test.jpeg',30,80,0,90);
 
@@ -61,6 +68,18 @@
 
     $pdf->Cell(100,10,'District:',1,0);
     $pdf->Cell(0,10,$plate_license,1,1);
+
+    $pdf->Cell(0,10,'All citations must be paid online.',0,1);
+    $pdf->Cell(0,20,'If you have any questions or concerns, or need assistance, please contact me at +673 7135636 ',0,1);
+    $pdf->Cell(0,5,'or by email support teams at 19ftt1185@student.pb.edu.bn.',0,1);
+    $pdf->Cell(0,20,'This was an automated message sent on behalf of your local Department of Motor Vehicles. Do not reply to this message and do not mail in your checks to us.',0,1);
+    $pdf->Cell(0,5,'Do not reply to this message and do not mail in your checks to us.',0,1);
+    
+    
+
+
+
+
 
     $pdf->Output();
 ?>
