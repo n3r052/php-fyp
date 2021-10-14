9d3aacf2ae13a02e7b14d218f55c01868ad7a361
19ftt1185 <19ftt1185@student.pb.edu.bn>
2021-10-14 19:57:09 +0800

    fix nav

--- documents.php
+++ documents.php
@@ -141,16 +141,16 @@
                 <div class="dropdown-header noti-title">
                   <h6 class="text-overflow m-0">Welcome!</h6>
                 </div>
-                <a href="#!" class="dropdown-item">
+                <a href="profile.php" class="dropdown-item">
                   <i class="ni ni-single-02"></i>
                   <span>My profile</span>
                 </a>
-                <a href="#!" class="dropdown-item">
+                <!-- <a href="#!" class="dropdown-item">
                   <i class="ni ni-settings-gear-65"></i>
                   <span>Settings</span>
-                </a>
+                </a> -->
                 <div class="dropdown-divider"></div>
-                <a href="#!" class="dropdown-item">
+                <a href="modal/logout.php" class="dropdown-item">
                   <i class="ni ni-user-run"></i>
                   <span>Logout</span>
                 </a>
