29498aec10745cf938f0c54f5b2eb4644811c421
19ftt1185 <19ftt1185@student.pb.edu.bn>
2021-10-15 18:14:36 +0800

    fix nav

--- officer/index.php
+++ officer/index.php
@@ -136,7 +136,7 @@
               </li>
   
               <li class=\"nav-item\">
-                <a class=\"nav-link\" href=\"report.php\">
+                <a class=\"nav-link\" href=\"../report.php\">
                   <i class=\"ni ni-bullet-list-67 text-default\"></i>
                   <span class=\"nav-link-text\">Compose Report</span>
                 </a>
