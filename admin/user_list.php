e4b99d919bff51e16b3fe722cedf77449ef49326
19ftt1185 <19ftt1185@student.pb.edu.bn>
2021-10-15 18:35:24 +0800

    fix nav

--- admin/user_list.php
+++ admin/user_list.php
@@ -82,55 +82,6 @@
                 </a>
               </li>
                 ";
-              }elseif($position == "operator"){
-               // echo "am operator";
-
-                echo " 
-                <li class=\"nav-item\">
-                <a class=\"nav-link\" href=\"operator/index.php\">
-                  <i class=\"ni ni-tv-2 text-primary\"></i>
-                  <span class=\"nav-link-text\">Dashboard</span>
-                </a>
-              </li>
-  
-              <li class=\"nav-item\">
-                <a class=\"nav-link\" href=\"report.php\">
-                  <i class=\"ni ni-bullet-list-67 text-default\"></i>
-                  <span class=\"nav-link-text\">Compose Report</span>
-                </a>
-              </li>
-  
-              <li class=\"nav-item\">
-                <a class=\"nav-link\" href=\"documents.php\">
-                  <i class=\"ni ni-bullet-list-67 text-default\"></i>
-                  <span class=\"nav-link-text\">Documents</span>
-                </a>
-              </li>
-  
-              <li class=\"nav-item\">
-                <a class=\"nav-link\" href=\"device.php.\">
-                  <i class=\"ni ni-pin-3 text-primary\"></i>
-                  <span class=\"nav-link-text\">Device</span>
-                </a>
-              </li>
-                ";
-              }elseif($position == "officer"){
-                //echo "am officer";
-                echo " 
-                <li class=\"nav-item\">
-                <a class=\"nav-link\" href=\"officer/index.php\">
-                  <i class=\"ni ni-tv-2 text-primary\"></i>
-                  <span class=\"nav-link-text\">Dashboard</span>
-                </a>
-              </li>
-  
-              <li class=\"nav-item\">
-                <a class=\"nav-link\" href=\"report.php\">
-                  <i class=\"ni ni-bullet-list-67 text-default\"></i>
-                  <span class=\"nav-link-text\">Compose Report</span>
-                </a>
-              </li>
-                ";
               }
             ?>
 
